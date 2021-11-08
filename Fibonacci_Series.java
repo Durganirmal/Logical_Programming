4f75fa88a7420801577476842aa618cdd0f48e50
Durga Nirmal <durganirmal123@gmail.com>
2021-11-08 16:38:49 +0530

    Added : Program for Fibonacci Series

--- Fibonacci_Series.java
+++ Fibonacci_Series.java
@@ -1,10 +1,21 @@
 package com.Logical_Program;
-
+import java.util.Scanner;
 public class Fibonacci_Series {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to logical Programming !!" );
-
+		System.out.println("Enter the number");
+		Scanner scanner=new Scanner(System.in);
+		 int num=scanner.nextInt();
+		 scanner.close();
+		 int a=0, b=1,c;
+		 for(int i=1; i<=num; i++) {
+			 c=a+b;
+			 System.out.print(c+ ", ");
+			 a=b;
+			 b=c;
+		 }
+		 
+	}
 	}
 
-}
+
